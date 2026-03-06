5768a801b1f980fd3feaf2d0716e4e83cb7d0754
ikapl <ilker.kaplan@lmdsio.fr>
2026-03-06 09:06:19 +0100

    2eme push

--- src/main/java/com/example/projetjfx/HelloController.java
+++ src/main/java/com/example/projetjfx/HelloController.java
@@ -3,16 +3,20 @@ package com.example.projetjfx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
     private Button helloButton;
+    @FXML
+    private TextField nameField;
 
     @FXML
     protected void onHelloButtonClick() {
 
         welcomeText.setText("Welcome to JavaFX Application!");
-        helloButton.setText("Bravo");
+        helloButton.setText("Bravo "+nameField.getText()+"!");
     }
 }
