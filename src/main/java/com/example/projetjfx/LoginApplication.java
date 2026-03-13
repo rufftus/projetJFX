7e25ae35e3167963baf18a5de2079173687658f5
ikapl <ilker.kaplan@lmdsio.fr>
2026-03-13 08:20:22 +0100

    bonus

--- src/main/java/com/example/projetjfx/LoginApplication.java
+++ src/main/java/com/example/projetjfx/LoginApplication.java
@@ -19,5 +19,7 @@ public class LoginApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+
+
     }
 }
--- src/main/java/com/example/projetjfx/LoginController.java
+++ src/main/java/com/example/projetjfx/LoginController.java
@@ -1,5 +1,4 @@
 package com.example.projetjfx;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -15,32 +14,45 @@ public class LoginController {
     @FXML
     private Button logoutButton;
 
+    private int erreur=0;
+
+
     @FXML
     protected void onLoginButtonClick() {
-
         String login = loginField.getText();
         String password =passwordField.getText();
         loginText.setText("Veuillez saisir votre identifiants");
         loginText.setStyle("fx-text-fill: black");
-
         if(!password.equals("mdp")){
+            erreur=erreur+1;
             loginText.setText("Identifiants incorrects");
             loginText.setStyle("-fx-text-fill: red");
         } else{
-            loginText.setText("Hello "+login);
-            loginText.setStyle("-fx-text-fill: green;-fx-border-color: green; -fx-border-width: 2px");
+            if(erreur>=3){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Vous avez fait 3 erreurs");
+                alert.showAndWait();
+                loginButton.setDisable(true);
+            }
+            else{
+                loginText.setText("Hello "+login);
+                loginText.setStyle("-fx-text-fill: green;-fx-border-color: green; -fx-border-width: 2px");
+                logoutButton.setDisable(false);
+                loginButton.setDisable(true);
+            }
         }
 
     }
     @FXML
     protected void onLogoutButtonClick() {
-        logoutButton.setDisable(true);
-        loginButton.setDisable(false);
         String login = loginField.getText();
         String password =passwordField.getText();
         loginField.clear();
         passwordField.clear();
         loginText.setText("Veuillez saisir votre identifiants");
         loginText.setStyle("fx-text-fill: black");
+        logoutButton.setDisable(true);
+        loginButton.setDisable(false);
+        erreur=0;
     }
 }
